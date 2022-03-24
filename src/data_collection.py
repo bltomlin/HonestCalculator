e79ab53edb52810ef20885cc9bfd1d8ba9a8e869
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-23 23:11:45 -0700

    Update data_collection.py

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,20 +1,35 @@
-def data_collection():
-    response = ''
-    msg_0 = "Enter an equation"
+def data_collection(user_input):
+    x_var = int(user_input[0])
+    oper = str(user_input[1])
+    y_var = int(user_input[2])
+    return x_var, oper, y_var
+
+def test(x_var, oper, y_var):
     msg_1 = "Do you even know what numbers are? Stay focused!"
     msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+    response = ''
     while response != 'exit':
-        print(msg_0)
-        user_input = input().split()
-        x_var = float(user_input[0])
-        oper = str(user_input[1])
-        y_var = float(user_input[2])
-
-        if type(x_var) == 'int' and type(y_var) == 'int':
-            if '+-*/' in oper:
+        if type(x_var) is int and type(y_var) is int:
+            if oper in ['+', '-', '*', '/']:
                 response = 'exit'
             else:
                 print(msg_2)
+                quit()
         else:
             print(msg_1)
-data_collection()
+            quit()
+
+
+def grab_input(msg_0):
+    print(msg_0)
+    user_input = input().split()
+    return user_input
+
+def main():
+    msg_0 = "Enter an equation"
+    user_input = grab_input(msg_0)
+    data_collection(user_input)
+    x_var, oper, y_var = data_collection(user_input)
+    test(x_var, oper, y_var)
+
+main()
