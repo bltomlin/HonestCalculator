af4aabd79648415d7cd5b57133d53f6b8d41ec6d
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-25 23:49:01 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
+"""
+This file contains the main entry point for the program that
+dictates all logical flow.
+"""
+
+
 def honest_calculator():
+    """
+    Entry point for program.
+    
+    :return: void
+    """
     print("Enter an equation")
     user_input = grab_input()
     x_var, oper, y_var = data_collection(user_input)
     test_equation(x_var, oper, y_var)
+
 
 if __name__ == '__main__':
     honest_calculator()
