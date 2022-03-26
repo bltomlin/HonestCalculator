e2c93ea51b29ca8f9eb31418d18d62716c030e56
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-26 00:00:56 -0700

    Update test_equation.py

--- src/test_equation.py
+++ src/test_equation.py
@@ -1,7 +1,22 @@
+"""
+This file contains a function used to validate an user entered
+equation.
+"""
+
+
 def test_equation(x_var, oper, y_var):
+    """
+    Validate the user's entered equation to ensure they are writing
+    equations correctly.
+
+    :param x_var: the float x variable of the equation 
+    :param oper: the str operator of the equation
+    :param y_var: the float y variable of the equation
+    """
     msg_1 = "Do you even know what numbers are? Stay focused!"
     msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
     response = ''
+    
     while response != 'exit':
         if type(x_var) is float and type(y_var) is float:
             if oper in ['+', '-', '*', '/']:
