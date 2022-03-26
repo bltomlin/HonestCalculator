2161703b432b48ac14747d8b26d141e702a7852f
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-25 23:32:48 -0700

    Update data_collection.py

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,34 +1,45 @@
 def data_collection(user_input):
-    x_var = int(user_input[0])
-    oper = str(user_input[1])
-    y_var = int(user_input[2])
+    x_var = type_check(user_input[0])
+    oper = user_input[1]
+    y_var = type_check(user_input[2])
     return x_var, oper, y_var
+    
+def type_check(some_input):
+    try: 
+        some_input = float(some_input)
+        return some_input
+    except ValueError:
+        some_input = str(some_input)
+        return some_input
+        
 
 def test(x_var, oper, y_var):
     msg_1 = "Do you even know what numbers are? Stay focused!"
     msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
     response = ''
     while response != 'exit':
-        if type(x_var) is int and type(y_var) is int:
+        if type(x_var) is float and type(y_var) is float:
             if oper in ['+', '-', '*', '/']:
                 response = 'exit'
             else:
                 print(msg_2)
-                quit()
+                print("Enter an equation")
+                new_input = grab_input()
+                x_var, oper, y_var = data_collection(grab_input())
         else:
             print(msg_1)
-            quit()
+            print("Enter an equation")
+            new_input = grab_input()
+            x_var, oper, y_var = data_collection(grab_input())
 
 
-def grab_input(msg_0):
-    print(msg_0)
+def grab_input():
     user_input = input().split()
     return user_input
 
 def main():
-    msg_0 = "Enter an equation"
-    user_input = grab_input(msg_0)
-    data_collection(user_input)
+    print("Enter an equation")
+    user_input = grab_input()
     x_var, oper, y_var = data_collection(user_input)
     test(x_var, oper, y_var)
 
