d8fdd464968ca80be751ec420a2e48d1d9668951
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-26 00:04:54 -0700

    Update type_check.py

--- src/type_check.py
+++ src/type_check.py
@@ -1,4 +1,16 @@
+"""
+This file contains a function used to assign the correct type
+on a user entered equation.
+"""
+
+
 def type_check(some_input):
+    """
+    Assign the correct type on a user entered equation to avoid
+    accomodate a failure to input numbers in an equation.
+
+    :param some_input: an element from the user input list.
+    """
     try: 
         some_input = float(some_input)
         return some_input
