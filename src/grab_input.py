2fe4e3ee49168f27b4248bd2901ef10586a74a60
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-25 23:52:09 -0700

    Update grab_input.py

--- src/grab_input.py
+++ src/grab_input.py
@@ -1,3 +1,12 @@
+"""
+This module contains the input function.
+"""
+
 def grab_input():
+    """
+    Grabs users input and splits it into a string list.
+
+    :return: string list comprised of user input
+    """
     user_input = input().split()
     return user_input
