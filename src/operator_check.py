ca1d1598b20d9df51b1f352fdf83b81cac1c3747
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-26 13:58:47 -0700

    Create operator_check.py

---
+++ src/operator_check.py
@@ -0,0 +1,33 @@
+"""
+This file contains a function used to validate the operator in equation.
+"""
+from src import data_collection, grab_input, test_equation
+
+
+def operator_check(x_var, oper, y_var):
+    """
+    Check's the operator of the entered equation.
+    :param x_var: the float x variable of the equation 
+    :param oper: the str operator of the equation
+    :param y_var: the float y variable of the equation
+    """
+    response = ''
+    while response != 'exit':
+        if oper == '+':
+            print(x_var + y_var)
+            response = 'exit'
+        elif oper == '-':
+            print(x_var - y_var)
+            response = 'exit'
+        elif oper == '*':
+            print(x_var * y_var)
+            response = 'exit'
+        else:
+            if oper == '/' and y_var != 0:
+                print(x_var / y_var)
+                response = 'exit'
+            else:
+                print("Yeah... division by zero. Smart move...")
+                print("Enter an equation")
+                x_var, oper, y_var = data_collection(grab_input())
+                test_equation(x_var, oper, y_var)
