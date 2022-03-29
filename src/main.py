7acc80dba9e7f3f5160b50c4c8d920ae551057e2
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-28 22:49:11 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,10 +11,13 @@ def honest_calculator():
 
     :return: void
     """
+    memory = 0
     print("Enter an equation")
     user_input = grab_input()
-    x_var, oper, y_var = data_collection(user_input)
-    test_equation(x_var, oper, y_var)
+    x_var, oper, y_var = data_collection(user_input, memory)
+    x_var, oper, y_var = test(x_var, oper, y_var)
+    result = operator_check(x_var, oper, y_var, memory)
+    store_result(result, memory)
 
 
 if __name__ == '__main__':
