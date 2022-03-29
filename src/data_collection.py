12db48f07dd28aa6d9dd41022414c691204464c5
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-28 22:49:09 -0700

    Update data_collection.py

--- src/data_collection.py
+++ src/data_collection.py
@@ -5,15 +5,16 @@ data to desired type in this project.
 from src import type_check
 
 
-def data_collection(user_input):
+def data_collection(user_input, memory):
     """
     Assigns data type based on compatibility.
     Ex: 'm' will be assigned str. 2.3 or 2 will be assigned float.
 
     :param user_input: list containing strings from user input
+    :param memory: user answer stored for future use
     :return: variables with correct types for program
     """
-    x_var = type_check(user_input[0])
+    x_var = type_check(user_input[0], memory)
     oper = user_input[1]
-    y_var = type_check(user_input[2])
+    y_var = type_check(user_input[2], memory)
     return x_var, oper, y_var
