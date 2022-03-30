d56ba9d09cf30fbfa03905faa61d77e5876c7e03
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-29 22:48:39 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ def honest_calculator():
     user_input = grab_input()
     x_var, oper, y_var = data_collection(user_input, memory)
     x_var, oper, y_var = test(x_var, oper, y_var)
+    check(x_var, y_var, oper)
     result = operator_check(x_var, oper, y_var, memory)
     store_result(result, memory)
 
