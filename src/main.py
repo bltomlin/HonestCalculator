1fef3a223beaeff522f9164658284cc91faa6328
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-30 12:52:19 -0700

    Replace functions calls with method
    Replaced bulk code with method for re-usability.

--- src/main.py
+++ src/main.py
@@ -12,12 +12,7 @@ def honest_calculator():
     :return: void
     """
     memory = 0
-    print("Enter an equation")
-    user_input = grab_input()
-    x_var, oper, y_var = data_collection(user_input, memory)
-    x_var, oper, y_var = test(x_var, oper, y_var)
-    check(x_var, y_var, oper)
-    result = operator_check(x_var, oper, y_var, memory)
+    result = call_functions()
     store_result(result, memory)
 
 
