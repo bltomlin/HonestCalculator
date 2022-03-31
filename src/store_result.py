e74fab5953348de0348a1266ea6f8831e4c36d8e
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-03-31 16:43:04 -0700

    Added code for responses

--- src/store_result.py
+++ src/store_result.py
@@ -7,12 +7,29 @@ def store_result(result, memory):
     :param result: answer from equation
     :param memory: double value user saves for future use
     """
+	msg = ["Are you sure? It is only one digit! (y / n)",
+	"Don't be silly! It's just one number! Add to the memory? (y / n)",
+	"Last chance! Do you really want to embarrass yourself? (y / n)"]
 	_exit = ''
 	while _exit != 'exit':
 		print("Do you want to store the result? (y / n):")
 		answer = input()
-		if answer == 'y':
-			memory = result
+		if answer == "y":
+			while is_one_digit(result) == True and answer == "y":
+				msg_index = 0
+				print(msg[msg_index])
+				answer = input()                
+				if answer == "y":
+					if msg_index < 2:
+						msg_index = msg_index + 1
+				else:
+					if answer == "n":
+						break
+					else:
+						pass
+				memory = result
+			else:
+				memory = result
 			print("Do you want to continue calculations? (y / n):")
 			answer = input()
 			if answer == 'y':
