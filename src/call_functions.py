ed2fcbab65038567c5a96c224948792f74c39529
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-02 00:24:42 -0700

    Comments explaining the function
    Added comments to help explain the purpose of the function.

--- src/call_functions.py
+++ src/call_functions.py
@@ -1,4 +1,11 @@
+"""
+This file contains a function used to call most of the main program.
+"""
 def call_functions(memory):
+	"""
+    Method for calling main functions in the program.
+    :param memory: double value user saves for future use
+    """
 	print("Enter an equation")
 	user_input = grab_input()
 	x_var, oper, y_var = data_collection(user_input, memory)
