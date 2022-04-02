ddc516a9ed518439e489844624ef9b6c8a1095cf
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-02 00:18:05 -0700

    Delete finish_calc.py

--- src/finish_calc.py
+++
@@ -1,9 +0,0 @@
-def finish_calc(memory):
-	print("Do you want to continue calculations? (y / n):")
-	answer = input()
-	if answer == 'y':
-		result = call_functions(memory)
-		return result
-	else:
-		return(memory)
-		_exit = 'exit'
