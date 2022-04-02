9e043cfd68875cff39574ae0fd9473ae4d92c444
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-02 00:25:28 -0700

    Added comments
    Added comments to explain the use of the function

--- src/is_one_digit.py
+++ src/is_one_digit.py
@@ -1,4 +1,11 @@
+"""
+This file contains a function used to validate how many digits are in the number.
+"""
 def is_one_digit(num):
+	"""
+    Check's if number is single digit.
+    :param num: value of integer being checked
+    """
 	output = ''
 	if num > -10 and num < 10 and num.is_integer():
 		output = True
